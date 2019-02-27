9ba63c8935c12129925ce7ef09f5812a5e74f160
Jonathan Seitz <mail@jonathanseitz.com>
2019-02-26 22:06:53 -0500

    Render the markdown text into a static string
    Allows for options custom components based on the slug or page type,
    falling back to a generic page.

--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,39 @@
-export default async (pageData) => {
-  console.debug(pageData);
+import marked from 'marked';
+import { highlightAuto } from 'highlight.js';
+import { html } from 'htm/preact';
+import render from 'preact-render-to-string';
+import { resolve } from 'path';
+import { promises as fs } from 'fs';
+import components from './components';
+
+marked.setOptions({
+  baseUrl: 'https://jonathanseitz.com/',
+  gfm: true,
+  tables: true,
+  smartypants: true,
+  smartLists: true,
+  highlight: code => highlightAuto(code).value,
+});
+
+export default async ({ body, attributes, outPath }) => {
+  const contentHTML = marked(body);
+  let pageComponent;
+  const {
+    title,
+    slug,
+    pageType,
+  } = attributes;
+  if (slug && slug in components) {
+    pageComponent = components[slug];
+  } else if (pageType in components) {
+    pageComponent = components[pageType];
+  } else {
+    pageComponent = components.page;
+  }
+  const pageHTML = render(html`
+    <${pageComponent} title=${title}>
+      ${contentHTML}
+    <//>
+  `);
+  console.debug(pageHTML);
 };
